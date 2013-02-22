67f872ca06dcf154f940a476e53d0f44c491bc6b
Michal Cab <majkl.cab@gmail.com>
2013-02-22 17:09:49 +0100

    Odebrání nepotřebných includů, přidání self.encoding, přidání dekódovací funkce decode()

--- researchr/research.py
+++ researchr/research.py
@@ -1,32 +1,33 @@
-import dict2xml
-from dict2xml import dict2xml
 import json
 import httplib
 
 class ResearchClass:
     def __init__(self):
         self.conn = httplib.HTTPConnection("researchr.org")
+		self.encoding = "UTF-8"
         
     def searchPublication(self, key):
         data = self.makeRequest(self, "search/publication", key)
-        JSON = json.loads(data.decode('UTF-8'))
+		return self.decode(data)
 
     def searchConference(self, key):
         data = self.makeRequest(self, "search/conference", key)
-        JSON = json.loads(data.decode('UTF-8'))
+		return self.decode(data)
         
     def getConference(self, key):
         data = self.makeRequest(self, "conference", key)
-        JSON = json.loads(data.decode('UTF-8'))
+		return self.decode(data)
 
     def getPublication(self, key):
         data = self.makeRequest(self, "publication", key)
-        JSON = json.loads(data.decode('UTF-8'))
-
+		return self.decode(data)
+			
     def getPerson(self, key):
         data = self.makeRequest("person", key)
         try:
-            JSON = json.loads(data.decode('UTF-8'))
+            JSON = json.loads(data.decode(self.encoding))
+		except:
+			print "Nastala chyba při načítání json."
     
     def getBibliography(self, key):
         data = self.makeRequest("bibliography", key)
@@ -36,6 +37,9 @@ class ResearchClass:
         self.conn.request("GET", "/api/%s/%s" % (term, key))
         res = self.conn.getresponse()
         if res.status != 200:
-            return res.reason
+            print res.reason
         data = res.read()
         return data
+		
+	def decode(data):
+		return json.loads(data.decode(self.encoding))
